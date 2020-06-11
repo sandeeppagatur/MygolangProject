dd66fc9ee97f87cb1eb9c660df1d4ad89af56a01
spagatur <spagatur@cisco.com>
2020-06-12 02:28:56 +0530

    added new folders

---
+++ go.mod
@@ -0,0 +1,5 @@
+module DemoProject/goSample/sandeeppagatur
+
+go 1.14
+
+require github.com/gorilla/mux v1.7.4
---
+++ models/Event.go
@@ -0,0 +1,7 @@
+package models
+type event struct {
+	ID          string `json:"ID"`
+	Title       string `json:"Title"`
+	Description string `json:"Description"`
+}
+
